cee8140314feca2eebe6231f86e10d092bef7807
liaotao <1053595189@qq.com>
2022-12-01 09:55:45 +0800

    init

---
+++ src/main/java/com/tiny/activiti/TinyActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.tiny.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyActivitiApplication.class, args);
+    }
+
+}
